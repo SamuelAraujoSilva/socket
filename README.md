0e04049edca34ed7a98dba65be3d75cc58bd6a4d
samuels <samuelsas.samuel18@gmail.com>
2019-02-19 08:46:52 -0300

    corrigindo bugs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# socket
+# Jogo com socket
+
+criar - cria o personagem
+
+info - buscar as informações do personagem
+
+buscar - pocurar por um monstro
+
+atacar - ataca o monstro encontrado
+
+ignorar - ignora o monstro (tem que buscar um novo)
+
+sair - sai do jogo
--- servidor.py
+++ servidor.py
@@ -3,59 +3,10 @@ import _thread
 import random
 import json
 
-serverPort = 5000
+serverPort = 8000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-
-agendas = {}
-
-# def task(connection, client):
-# 	agenda_name = "agenda_{}".format(client[1])
-# 	if not agenda_name in agendas:
-# 		agendas[agenda_name] = []
-# 	while True:
-# 		received = connection.recv(1024)
-# 		if received:
-# 			data = json.loads(received)
-# 			if data["ACTION"] == "CREATE":
-# 				agendas[agenda_name].append(data["ITEM"])
-# 				if data["ITEM"] in agendas[agenda_name]:
-# 					retorno = {"CODIGO": 200, "MESSAGE": "Item adicionado com sucesso"}
-# 					connection.send(json.dumps(retorno).encode("ascii"))
-# 				else:
-# 					retorno = {"CODIGO": 400, "MESSAGE": "Falha ao adicionar item"}
-# 					connection.send(json.dumps(retorno).encode("ascii"))
-# 			if data["ACTION"] == "UPDATE":
-# 				print(agendas[agenda_name][data["ITEM"]])
-# 				retorno = {"CODIGO": 200, "MESSAGE": "Item atualizado com sucesso"}
-# 				connection.send(json.dumps(retorno).encode("ascii"))
-# 				# for i, item in enumerate(agendas[agenda_name]):
-# 				# 	if item.id == data["ITEM"]["id"]:
-# 				# 		agendas[agenda_name][i] = data["ITEM"]
-# 				# if data["ITEM"] in agendas[agenda_name]:
-# 				# 	retorno = {"CODIGO": 200, "MESSAGE": "Item atualizado com sucesso"}
-# 				# 	connection.send(json.dumps(retorno).encode("ascii"))
-# 				# else:
-# 				# 	retorno = {"CODIGO": 400, "MESSAGE": "Falha ao atualizar item"}
-# 				# 	connection.send(json.dumps(retorno).encode("ascii"))
-# 			if data["ACTION"] == "LIST":
-# 				if len(agendas[agenda_name]) == 0:
-# 					retorno = {"CODIGO": 200, "MESSAGE": "Agenda vazia!"}
-# 					connection.send(json.dumps(retorno).encode("ascii"))
-# 				else:
-# 					connection.send(json.dumps(agendas[agenda_name]).encode("ascii"))
-# 			if data["ACTION"] == "DELETE":
-# 				for i, item in enumerate(agendas[agenda_name]):
-# 					if item.id == data["ID"]:
-# 						agendas[agenda_name].pop(i)
-# 				if not data["ITEM"] in agendas[agenda_name]:
-# 					retorno = {"CODIGO": 200, "MESSAGE": "Item removido com sucesso"}
-# 					connection.send(json.dumps(retorno).encode("ascii"))
-# 				else:
-# 					retorno = {"CODIGO": 400, "MESSAGE": "Falha ao remover item"}
-# 					connection.send(json.dumps(retorno).encode("ascii"))
-# 	connection.close()
 
 class Personagem():
 	def __init__(self, nome):
@@ -81,27 +32,25 @@ class Monstro():
 def task(connection, client):
 	personagem = None
 	monstro = None
+
 	received = json.loads(connection.recv(1024))
+
 	while received["option"] != "sair":
 		if received["option"] == "criar":
 			if not personagem:
-				nome = received["nome"]
-				personagem = Personagem(nome)
+				personagem = Personagem(client[1])
 				data = {"status": 200, "messagem": "Personagem criado com sucesso!"}
 			else:
-				data = {"status": 401, "messagem": "Você ja possui um personagem!"}
-			connection.send(json.dumps(data).encode("ascii"))
+				data = {"status": 400, "messagem": "Você ja possui um personagem!"}
 		elif received["option"] == "info":
 			if not personagem:
 				data = {"status": 400, "message": "Você ainda ñ criou um personagem"}
 			else:
 				data = {"status": 200, "personagem": {"nome": personagem.nome, "nivel": personagem.nivel, "vida": personagem.vida, "ataque": personagem.ataque}}
-			connection.send(json.dumps(data).encode("ascii"))
 		elif received["option"] == "buscar":
 			if not monstro:
 				monstro = Monstro()
 			data = {"status": 200, "monstro": {"nivel": monstro.nivel, "vida": monstro.vida, "ataque": monstro.ataque}}
-			connection.send(json.dumps(data).encode("ascii"))
 		elif received["option"] == "atacar":
 			if personagem:
 				if monstro:
@@ -116,7 +65,6 @@ def task(connection, client):
 					data = {"status": 400, "messagem": "Nenhuma monstro a vista. Faça uma busca antes de atacar"}
 			else:
 				data = {"status": 400, "messagem": "Você ainda ñ criou um personagem"}
-			connection.send(json.dumps(data).encode("ascii"))
 		elif received["option"] == "ignorar":
 			if not personagem:
 				data = {"status": 400, "messagem": "Você ainda ñ criou um personagem"}
@@ -126,14 +74,11 @@ def task(connection, client):
 				else:
 					monstro = None
 					data = {"status": 200, "messagem": "Monstro muito forte! vamos para o proximo"}
-			connection.send(json.dumps(data).encode("ascii"))
 		else:
 			data = {"error": 400, "messagem": "opção invalida!"}
-			connection.send(json.dumps(data).encode("ascii"))
+		connection.send(json.dumps(data).encode("utf8"))
 		received = json.loads(connection.recv(1024))
-	data = {"message": "Ate mais!"}
-	connection.send(json.dumps(data).encode("ascii"))
-	print("cliente {} saiu".format(client[1]) )
+	print("{} saiu".format(client[1]))
 	connection.close()
 
 print ("O servidor está pronto para receber!")
